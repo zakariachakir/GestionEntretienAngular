62769c3f7d986b349a5e48d950ea0e5a41bf2fd2
zakaria <zikochakir@gmail.com>
2020-07-19 00:49:02 +0100

    initial commit

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const app = express();
 const path = path();
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 54988);
- //PathLocationStrategie
 app.get('/*', function (req, res) {
 res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
+});
 console.log('im running');

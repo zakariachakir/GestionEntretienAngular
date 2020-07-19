0804d046ac0600ff532d5c3fdf1fb8341aea13f3
zakaria <zikochakir@gmail.com>
2020-07-19 01:33:22 +0100

    initial commit

--- src/app/controller/service/urlconfiguration.service.ts
+++ src/app/controller/service/urlconfiguration.service.ts
@@ -7,7 +7,7 @@ export class UrlconfigurationService {
   /* !!!! ici vous devez changer l'url de communication avec BD sous la forme 'lien:Port' (sans ajout de http:;;) */
 private _urldb = 'fstentretien.herokuapp.com';
   /* !!!! ici vous devez l'url du front end sous la forme lien:Port (sans ajout de http://) */
-private _urlpageaccueil = 'entretienfst.herokuapp.com';
+private _urlpageaccueil = 'entretienfst.herokuapp.com/#';
 
 
   get urldb(): string {
